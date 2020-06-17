a35519d211513b20612eeeb005810fd03eed5a09
jespergp <jesper@jesper.no>
2020-06-17 13:49:43 +0200

    Added basic routing

---
+++ src/formbuilder/formbuilder-application/src/router/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+
+import index from '../views/index';
+import createForm from "../views/createForm";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={index} />
+      <Route path="/create-form" component={createForm} />
+    </Switch>
+  );
+}
