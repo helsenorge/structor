5f3446baef708cd1ecb7d7386fe8114ca441a76d
Frøydis Jørgensen <froydisjo@hotmail.com>
2020-06-17 09:50:20 +0200

    Add React-router-dom and base src for imports

--- src/dataexplorer/dataexplorer-application/src/index.tsx
+++ src/dataexplorer/dataexplorer-application/src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
