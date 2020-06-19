28257c32c2ef6d9afdb545680464081b31818765
CORP\hannes <hanneasoynes@gmail.com>
2020-06-19 13:24:59 +0200

    'Add section' functionality added

--- src/formbuilder/formbuilder-application/src/components/formBuilder/Section.tsx
+++ src/formbuilder/formbuilder-application/src/components/formBuilder/Section.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Button } from 'antd';
-import TitleInput from '../questionComponent/TitleInput';
+import InputField from '../questionComponent/InputField';
 import { PlusOutlined, MoreOutlined } from '@ant-design/icons';
 
-function Section() {
+type SectionProps = {
+    id: number;
+}
+
+function Section({id}:SectionProps) {
+    const [placeholder, setPlaceholder] = useState('Tittel...');
+    function findPlaceholder(){
+        if (id == 0){
+            setPlaceholder('Tittel...');
+            return;
+        } 
+        setPlaceholder('Seksjonstittel...');
+    }
+
+    useEffect(() => {
+        console.log("I seksjon: ", id);
+        findPlaceholder();
+    });
+
     return (
         <div
             style={{
                 margin: '10px',
                 padding: '10px',
-                backgroundColor: '#FAFAFA',
+                backgroundColor: 'var(--color-base-1)',
                 width: '95%',
+                display: 'inline-block'
             }}
         >
             <Row>
                 <Col span={24}>
                     <div style={{ display: 'inline' }}>
-                        <TitleInput></TitleInput>
+                        <InputField placeholder={placeholder} />
                     </div>
                     <div style={{ display: 'inline' }}>
                         <Button
                             type="link"
                             shape="circle"
-                            style={{ color: '#A61E7B' }}
+                            style={{ color: 'var(--primary-1)' }}
                             icon={<MoreOutlined />}
                         />
                     </div>
@@ -34,8 +53,8 @@ function Section() {
                         type="primary"
                         shape="circle"
                         style={{
-                            backgroundColor: '#A61E7B',
-                            borderColor: '#A61E7B',
+                            backgroundColor: 'var(--primary-1)',
+                            borderColor: 'var(--primary-1)',
                         }}
                         icon={<PlusOutlined />}
                         size="large"
--- src/formbuilder/formbuilder-application/src/views/createForm.tsx
+++ src/formbuilder/formbuilder-application/src/views/createForm.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import { Row, Col } from 'antd';
+import React, { useState } from 'react';
+import { Row, Col, Button } from 'antd';
 import NavBar from '../components/formBuilder/NavBar';
 import Section from '../components/formBuilder/Section';
+import './createForm.css';
 
 function CreateForm(): JSX.Element {
+    
+    const [i, setI] = useState(0);
+    const [sections, setSections] = useState([<Section key={i} id={i} />]);
+    
+    function addNewSection(){
+        setI(i+1);
+        setSections(sections => [...sections, <Section key={i+1} id={i+1} />]);
+    }
+    
     return (
-        <div style={{ backgroundColor: '#C7C7C7', height: '100vh' }}>
-            <Row style={{ height: '56px' }}>
-                <Col span="24">
-                    <NavBar></NavBar>
+        <div style={{ backgroundColor: 'var(--color-base-2)', height: '100vh' }}>
+            <Row>
+                <Col span={24}>
+                    <NavBar/>
                 </Col>
             </Row>
+            <Row style={{margin:'61px 0 0 0'}}>
+                <Col span={24}>
+                    {sections}
+                </Col>
+            </Row>
+
+
             <Row>
-                <Col span="24">
-                    <Section></Section>
+                <Col span={24}>
+                    <div style={{
+                         margin: '10px',
+                         display: 'inline-block'
+                    }}>
+                        <Button className="section-button" type="dashed" ghost size="large" onClick={addNewSection}>
+                            Legg til ny seksjon
+                        </Button>
+                    </div>
                 </Col>
             </Row>
         </div>
