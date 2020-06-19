67f0549ce5a349af3bb87a0c57864e0c0dc34590
EnvyPhD <twiller3@hotmail.com>
2020-06-19 15:51:41 +0200

    Add: Created searchbox. Add Filtering. Add dynamic card generation. Moved to component

---
+++ src/dataexplorer/dataexplorer-application/src/components/MyPatients/MyPatients.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SearchBox from '../../components/Search/Search'
+const MyPatients = () => {
+    return <div>
+        <SearchBox />
+    </div>
+}
+export default MyPatients;
