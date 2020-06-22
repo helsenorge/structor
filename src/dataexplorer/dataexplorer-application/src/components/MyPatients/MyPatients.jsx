1fc988bf9eb705423281b64b99368642ae70e844
EnvyPhD <twiller3@hotmail.com>
2020-06-22 10:01:36 +0200

    Add: Searchbox with filter on name. TS. Format.

--- src/dataexplorer/dataexplorer-application/src/components/MyPatients/MyPatients.jsx
+++ src/dataexplorer/dataexplorer-application/src/components/MyPatients/MyPatients.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import SearchBox from '../../components/Search/Search'
+import SearchBox from '../../components/Search/Search.tsx';
 const MyPatients = () => {
-    return <div>
-        <SearchBox />
-    </div>
-}
+    return (
+        <div>
+            <SearchBox />
+        </div>
+    );
+};
 export default MyPatients;
