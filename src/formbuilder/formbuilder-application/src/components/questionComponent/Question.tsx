8f255044a2e956dd9b51c65d8945a1b821a41d46
CORP\riqran <iqran@hotmail.no>
2020-06-22 13:15:42 +0200

    Implemented AnswerComponent in QuestionComponent

--- src/formbuilder/formbuilder-application/src/components/questionComponent/Question.tsx
+++ src/formbuilder/formbuilder-application/src/components/questionComponent/Question.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Input,Row, Col, Button } from 'antd';
-import { PlusOutlined,MoreOutlined } from '@ant-design/icons';
+import React, { ReactElement } from 'react';
+import { Input, Row, Col, Button } from 'antd';
+import { PlusOutlined, MoreOutlined } from '@ant-design/icons';
 import './QuestionComponents.css';
+import AnswerComponent from '../answerComponent/AnswerComponent';
 const { TextArea } = Input;
 
-function Question() {
+const Question: React.FC = (): ReactElement => {
     return (
         <div>
             <Row>
-                <Col span ={23}>
+                <Col span={23}>
                     <Row>
-                        <Col span = {23} >
+                        <Col span={23}>
                             <div style={{ display: 'inline' }}>
-                                <div style={{ width: '60%', display: 'inline-block', padding: '5px' }}>
+                                <div
+                                    style={{
+                                        width: '60%',
+                                        display: 'inline-block',
+                                        padding: '5px',
+                                    }}
+                                >
                                     <TextArea
-                                        rows = {2}
+                                        rows={2}
                                         placeholder="Spørsmål...."
                                     />
                                 </div>
@@ -23,18 +30,24 @@ function Question() {
                     </Row>
 
                     <Row>
-                        <Col span = {23}>
+                        <Col span={23}>
                             <div style={{ display: 'inline' }}>
-                                <div style={{ width: '60%', display: 'inline-block', padding: '5px' }}>
+                                <div
+                                    style={{
+                                        width: '60%',
+                                        display: 'inline-block',
+                                        padding: '5px',
+                                    }}
+                                >
                                     <TextArea
-                                        rows = {4}
+                                        rows={4}
                                         placeholder="Forklarende tekst....."
                                     />
                                 </div>
                             </div>
                         </Col>
                     </Row>
-
+                    <AnswerComponent></AnswerComponent>
                     <Row>
                         <Col span={23}>
                             <Button
@@ -49,13 +62,7 @@ function Question() {
                             />
                         </Col>
                     </Row>
-                        {/*
-                        AnswerComponent settes inn her for hvert spørsmål
-                        <Row>
-                            <Answer></Answer>
-                        </Row>
-                        */}
-                        </Col>
+                </Col>
 
                 <Col span={1}>
                     <div style={{ display: 'inline' }}>
@@ -70,6 +77,6 @@ function Question() {
             </Row>
         </div>
     );
-}
+};
 
 export default Question;
