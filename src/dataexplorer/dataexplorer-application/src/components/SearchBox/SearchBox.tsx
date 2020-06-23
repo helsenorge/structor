0d3e8789e882a989e8f3076cfffcfdeca75e9a74
EnvyPhD <twiller3@hotmail.com>
2020-06-23 09:41:58 +0200

    Fix: Fixed code-hang of useEffect and eslint error. Moved dataconst

--- src/dataexplorer/dataexplorer-application/src/components/SearchBox/SearchBox.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/SearchBox/SearchBox.tsx
@@ -3,6 +3,51 @@ import { Card } from 'antd';
 import Search from 'antd/lib/input/Search';
 import { IPatient } from 'types/IPatient';
 
+const cards: IPatient[] = [
+    {
+        id: 1,
+        name: 'Erling',
+        description: ['tall', 'male', 'sick'],
+        age: 98,
+        isHuman: true,
+    },
+    {
+        id: 2,
+        name: 'Frøydis',
+        description: ['short', 'male', 'healthy'],
+        age: 27,
+        isHuman: false,
+    },
+    {
+        id: 3,
+        name: 'Marius',
+        description: ['short', 'male', ''],
+        age: 14,
+        isHuman: true,
+    },
+    {
+        id: 4,
+        name: 'Rebekka',
+        description: ['tall', 'female', 'sick'],
+        age: 16,
+        isHuman: true,
+    },
+    {
+        id: 5,
+        name: 'Jenny',
+        description: ['tall', 'female', 'sick'],
+        age: 42,
+        isHuman: false,
+    },
+    {
+        id: 6,
+        name: 'Jens',
+        description: ['tall', 'female', 'sick'],
+        age: 7,
+        isHuman: true,
+    },
+];
+
 const SearchBox = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState<IPatient[]>([]);
@@ -10,57 +55,12 @@ const SearchBox = () => {
     const handleChange = (event: any) => {
         setSearchTerm(event.target.value);
     };
-    const cards: IPatient[] = [
-        {
-            id: 1,
-            name: 'Erling',
-            description: ['tall', 'male', 'sick'],
-            age: 98,
-            isHuman: true,
-        },
-        {
-            id: 2,
-            name: 'Frøydis',
-            description: ['short', 'male', 'healthy'],
-            age: 27,
-            isHuman: false,
-        },
-        {
-            id: 3,
-            name: 'Marius',
-            description: ['short', 'male', ''],
-            age: 14,
-            isHuman: true,
-        },
-        {
-            id: 4,
-            name: 'Rebekka',
-            description: ['tall', 'female', 'sick'],
-            age: 16,
-            isHuman: true,
-        },
-        {
-            id: 5,
-            name: 'Jenny',
-            description: ['tall', 'female', 'sick'],
-            age: 42,
-            isHuman: false,
-        },
-        {
-            id: 6,
-            name: 'Jens',
-            description: ['tall', 'female', 'sick'],
-            age: 7,
-            isHuman: true,
-        },
-    ];
 
     useEffect(() => {
         const results: IPatient[] = cards.filter((card) =>
             card.name.toLowerCase().includes(searchTerm),
         );
         setSearchResults(results);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searchTerm]);
 
     return (
