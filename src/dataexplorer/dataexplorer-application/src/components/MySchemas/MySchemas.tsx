f31a353afc3ba435990de8450f7590e82148b739
EnvyPhD <twiller3@hotmail.com>
2020-06-23 16:32:50 +0200

    Rm: Remove Searchbox for now as functionality changed

--- src/dataexplorer/dataexplorer-application/src/components/MySchemas/MySchemas.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/MySchemas/MySchemas.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import SearchBox from 'components/SearchBox/SearchBox';
+// import SearchBox from 'components/SearchBox/SearchBox';
 
 const MySchemas = (props: any) => {
     return (
-        <div className="searchBoxContainer" style={{ paddingTop: 100 }}>
-            <SearchBox placeholder="Finn et skjema" />
-        </div>
+        <div className="searchBoxContainer" style={{ paddingTop: 100 }}></div>
     );
 };
 
