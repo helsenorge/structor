4730406a726c2eb59c3f73bd15b2660792dd771d
Frøydis Jørgensen <froydisjo@hotmail.com>
2020-06-24 08:37:25 +0200

    Add premade fhir interfaces

--- src/dataexplorer/dataexplorer-application/src/types/IQuestionnaireResponse.ts
+++ src/dataexplorer/dataexplorer-application/src/types/IQuestionnaireResponse.ts
@@ -1,3 +1,27 @@
-export interface IQuestionnaireResponses {
-    total: number;
+import { QuestionnaireResponseItem } from './fhirTypes/fhir';
+import { Dayjs } from 'dayjs';
+
+export interface IQuestionnaireResponse {
+    entry: IEntry[];
+    id: string;
+}
+
+export interface IEntry {
+    fullUrl: string;
+    resource: IResource;
+}
+
+export interface IResource {
+    author: { reference: string };
+    authored: Dayjs;
+    id: string;
+    item: QuestionnaireResponseItem[];
+    meta: IMeta;
+    status: string;
+}
+
+export interface IMeta {
+    lastUpdated: Dayjs;
+    versionId: string;
+    resourceType: string;
 }
