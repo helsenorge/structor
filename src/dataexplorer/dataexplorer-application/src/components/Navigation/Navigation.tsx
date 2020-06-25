a1dce8f434aad5aa630e5d098bc1d821132ca35c
EnvyPhD <twiller3@hotmail.com>
2020-06-25 13:22:18 +0200

    Add: Breadcrumb navbar. Remove: Sidebar. Change: Colorscheme

--- src/dataexplorer/dataexplorer-application/src/components/Navigation/Navigation.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/Navigation/Navigation.tsx
@@ -1,129 +1,87 @@
-import React from 'react';
-import { Menu, Layout, Avatar } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Avatar } from 'antd';
+import Breadcrumbs from './Breadcrumbs';
 import 'antd/dist/antd.css';
-import Dashboard from '../Dashboard/Dashboard';
 import MySchemas from '../MySchemas/MySchemas';
 import MyPatients from '../MyPatients/MyPatients';
-import { Switch, Route, Link, withRouter } from 'react-router-dom';
-import MenuItem from 'antd/lib/menu/MenuItem';
+import { Switch, Route, withRouter, Link } from 'react-router-dom';
 import ListView from '../PatientInfo/ListView/ListView';
-import SchemaView from 'components/PatientInfo/SchemaView/SchemaView';
+import SchemaView from 'components/MyPatients/SchemaView/SchemaView';
+import { UserOutlined } from '@ant-design/icons';
+import PatientInfo from '../MyPatients/PatientInfo/PatientInfo';
 
-const { Header, Content, Sider, Footer } = Layout;
-const data = {
-    items: [
-        {
-            id: 1,
-            url: '/',
-            title: 'Hjem',
-        },
-        {
-            id: 2,
-            url: '/Pasient',
-            title: 'Finn en Pasient',
-        },
-        {
-            id: 3,
-            url: '/Pasient',
-            title: 'Lag Nytt Skjema',
-        },
-        {
-            id: 4,
-            url: '/Pasient',
-            title: 'Se Mottatte Skjema',
-        },
-        {
-            id: 5,
-            url: '/Pasient',
-            title: 'Visualiser Data',
-        },
-    ],
-};
+const { Header, Content } = Layout;
 
-const Navigation = () => (
+const Navigation = (props: any) => (
     <>
         <Layout>
-            <Sider breakpoint="xs" collapsedWidth="0">
-                <div className="logo">
-                    <h1
-                        style={{
-                            color: 'white',
-                            paddingLeft: 20,
-                            paddingTop: 20,
-                            paddingBottom: 18,
-                            marginBottom: 0,
-                        }}
-                    >
-                        Datautforskeren
-                    </h1>
-                </div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    {data.items.map(({ id, url, title }) => (
-                        <MenuItem style={{ marginTop: 0 }} key={id}>
-                            <Link to={url}> {title} </Link>
-                        </MenuItem>
-                    ))}
-                </Menu>
-            </Sider>
             <Layout>
                 <Header
                     className="site-layout-sub-header-background"
                     style={{
-                        padding: 0,
                         position: 'fixed',
                         zIndex: 1,
                         width: '100%',
                         flexDirection: 'row',
                         display: 'flex',
-                        justifyContent: 'flex-end',
+                        justifyContent: 'space-between',
                         alignItems: 'center',
+                        backgroundColor: '#EDF5E1',
+                        borderBottom: 'solid lightgrey thin',
+                        paddingLeft: 15,
                     }}
                 >
-                    <Avatar
-                        shape="square"
-                        size="large"
-                        src="https://i.pinimg.com/236x/6c/ab/cc/6cabccf7f0ebb599cb4fd1dd783877dd--wallpaper-house-gregory-house.jpg"
+                    <Link to="/">
+                        <h1
+                            style={{
+                                color: 'black',
+                                margin: 0,
+                                paddingRight: 20,
+                                borderRight: 'solid lightgrey thin',
+                            }}
+                        >
+                            Datautforskeren
+                        </h1>
+                    </Link>
+                    <Breadcrumbs
+                        path={props.history}
+                        style={{ flexGrow: 2, paddingLeft: 20 }}
                     />
-                    <h1
+
+                    <div
+                        className="avatar-container"
                         style={{
                             color: 'white',
-                            paddingRight: 220,
-                            paddingLeft: 20,
+                            display: 'flex',
+                            flexDirection: 'row',
+                            borderLeft: 'solid lightgrey thin',
                         }}
                     >
-                        Dr. Gregory House
-                    </h1>
+                        <Avatar
+                            style={{
+                                marginLeft: 20,
+                                marginRight: 20,
+                                marginTop: 12,
+                            }}
+                            shape="square"
+                            size="large"
+                            // src="https://i.pinimg.com/236x/6c/ab/cc/6cabccf7f0ebb599cb4fd1dd783877dd--wallpaper-house-gregory-house.jpg"
+                            icon={<UserOutlined />}
+                        />
+                        <h1 style={{ margin: 0, marginRight: -40 }}>
+                            Dr. Gregory House
+                        </h1>
+                    </div>
                 </Header>
-                <Header
-                    className="patientHeader"
-                    style={{
-                        padding: 0,
-                        position: 'fixed',
-                        zIndex: 2,
-                        width: '100%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        alignContent: 'center',
-                        marginTop: 64,
-                        backgroundColor: 'lightgrey',
-                        height: 30,
-                        borderBottom: 'solid grey 1px',
-                    }}
-                >
-                    <p style={{ paddingRight: 200, paddingTop: 10 }}>
-                        Du undersøker nå: Erling van de Weijer
-                    </p>
-                </Header>
-                <Content style={{ margin: '24px 16px 0' }}>
+                <Content style={{ margin: '24px 16px 0', overflow: 'hidden' }}>
                     <div className="content">
                         <Switch>
-                            <Route exact path={'/'} component={Dashboard} />
                             <Route exact path="/Skjema" component={MySchemas} />
+                            <Route exact path="/" component={MyPatients} />
                             <Route
                                 exact
-                                path="/Pasient"
-                                component={MyPatients}
+                                path="/PasientInfo"
+                                component={PatientInfo}
                             />
                             <Route
                                 exact
@@ -138,7 +96,6 @@ const Navigation = () => (
                         </Switch>
                     </div>
                 </Content>
-                <Footer />
             </Layout>
         </Layout>
     </>
