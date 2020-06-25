ab35a82c9a9061f77a62169821721bd404b627a4
EnvyPhD <twiller3@hotmail.com>
2020-06-25 13:22:18 +0200

    Add: Breadcrumb navbar. Remove: Sidebar. Change: Colorscheme

---
+++ src/dataexplorer/dataexplorer-application/src/components/Navigation/Breadcrumbs.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Breadcrumb from 'antd/lib/breadcrumb';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
+
+const Breadcrumbs = (props: any) => {
+    return (
+        <>
+            <div className="Breadcrumbcontainer" style={props.style}>
+                <Breadcrumb>
+                    <Breadcrumb.Item href="/">
+                        <HomeOutlined />
+                        <span style={{ color: 'black' }}>Søk</span>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item href="Pasient">
+                        <UserOutlined />
+                        <span style={{ color: 'black' }}>
+                            Pasient - Erling van de Weijer
+                        </span>
+                    </Breadcrumb.Item>
+                    <Breadcrumb.Item>
+                        <span>Skjema 1 - 17.04.20</span>
+                    </Breadcrumb.Item>
+                </Breadcrumb>
+            </div>
+        </>
+    );
+};
+
+export default Breadcrumbs;
