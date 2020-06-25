f516abe057964f9ebb444a1701607d477a0573e6
Simen <simen-norrheim.larsen@capgemini.com>
2020-06-25 13:54:39 +0200

    Fixed radio, check and text input

--- src/formbuilder/formbuilder-application/src/components/answerComponent/TextInput.tsx
+++ src/formbuilder/formbuilder-application/src/components/answerComponent/TextInput.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import { Input } from 'antd';
 import './AnswerComponent.css';
 
-function TextInput(): JSX.Element {
-    return (
-        <div
-            style={{
-                marginTop: '20px',
-                width: '60%',
-                display: 'inline-block',
-            }}
-        >
-            <Input placeholder="Kort tekst svar..." disabled={true}></Input>
-        </div>
-    );
+const { TextArea } = Input;
+
+type TextInputProps = {
+    longAnswer: boolean;
+    maxLength: number;
+    placeholder?: string;
 };
 
+function TextInput({
+    longAnswer,
+    maxLength,
+    placeholder,
+}: TextInputProps): JSX.Element {
+    const textOutput = () => {
+        if (longAnswer) {
+            return (
+                <TextArea
+                    placeholder={placeholder}
+                    maxLength={maxLength}
+                    autoSize={{ minRows: 3, maxRows: 3 }}
+                ></TextArea>
+            );
+        } else {
+            return (
+                <Input placeholder={placeholder} maxLength={maxLength}></Input>
+            );
+        }
+    };
+    return textOutput();
+}
 export default TextInput;
