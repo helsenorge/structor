0b580dd8dc4ebc5c12753d00cb0268762bab9efd
Frøydis Jørgensen <froydisjo@hotmail.com>
2020-06-25 08:06:37 +0200

    Fetch questionnaire

--- src/dataexplorer/dataexplorer-application/src/components/SchemaResponse/SchemaResponse.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/SchemaResponse/SchemaResponse.tsx
@@ -3,42 +3,73 @@ import { Row } from 'antd';
 import 'dayjs/locale/nb';
 import Title from 'antd/lib/typography/Title';
 import useFetch from 'utils/hooks/useFetch';
-import { QuestionnaireResponse } from 'types/fhirTypes/fhir';
 import dayjs from 'dayjs';
+import { QuestionnaireResponse, Questionnaire } from 'types/fhirTypes/fhir';
 
-const SchemaResponse = () => {
-    const { response } = useFetch<QuestionnaireResponse>(
-        'fhir/QuestionnaireResponse/f201',
+const SchemaResponse = async () => {
+    const schemaResponse = await useFetch<QuestionnaireResponse>(
+        'fhir/QuestionnaireResponse/5',
     );
-    console.log(response);
+    const questionnairId = schemaResponse?.questionnaire?.reference?.slice(-2);
+    const { response: questionnaire } = useFetch<Questionnaire>(
+        'fhir/Questionnaire/' + questionnairId,
+    );
+    console.log(questionnairId);
+
+    console.log(schemaResponse);
+    console.log(questionnaire);
     dayjs.locale('nb');
-    const filledInDate = dayjs(response?.authored).format('DD/MM/YYYY HH:mm');
+    const filledInDate = dayjs(schemaResponse?.authored).format(
+        'DD/MM/YYYY HH:mm',
+    );
+    const firstText = schemaResponse
+        ? schemaResponse.item?.map((i) => i.text)
+        : ['empty'];
 
-    const itemQuestion = response?.item
-        ?.filter((i) => i.linkId === '3')
-        .flatMap((it) => it.item?.map((itt) => itt.text));
+    const secondText = schemaResponse
+        ? schemaResponse.item?.map((i) =>
+              i.item?.map((i) => i.text?.toString()),
+          )
+        : ['empty secondText'];
+    console.log(secondText);
+
+    /*     const itemQuestion = schemaResponse?.item?.filter((i) => i.linkId === '3');
     console.log(
-        itemQuestion && itemQuestion?.length > 1 ? itemQuestion[0] : 'error',
+        itemQuestion && itemQuestion?.length > 0 ? itemQuestion[0] : 'error',
     );
 
-    const itemAnswer = response?.item
+    const itemAnswer = schemaResponse?.item
         ?.filter((i) => i.linkId === '3')
         .flatMap((it) =>
             it.item?.map((itt) =>
                 itt.answer?.map((a) => a.valueString?.toString()),
             ),
         );
-    console.log(itemAnswer && itemAnswer?.length > 1 ? itemAnswer[0] : 'error');
+    console.log(itemAnswer && itemAnswer?.length > 0 ? itemAnswer[0] : 'error'); */
     return (
         <>
-            {response ? (
+            {schemaResponse && questionnaire ? (
                 <>
                     <Row gutter={40} justify={'center'}>
-                        <Title level={3}>Skjema id: {response?.id}</Title>
+                        <Title level={3}>Skjema id:{schemaResponse.id}</Title>
                     </Row>
                     <Row justify={'center'}>
                         Skjemaet ble fyllt ut: {filledInDate}
                     </Row>
+                    {firstText &&
+                        firstText.map((title, i) => (
+                            <Row justify={'center'} key={i}>
+                                <Title level={4} key={i}>
+                                    {title}
+                                </Title>
+                            </Row>
+                        ))}
+                    {/*                     {secondText &&
+                        secondText.map((question, i) => (
+                            <Row justify={'center'} key={i}>
+                                <Col key={i}>{question}</Col>
+                            </Row>
+                        ))} */}
                 </>
             ) : (
                 <Title level={4}>Venter...</Title>
