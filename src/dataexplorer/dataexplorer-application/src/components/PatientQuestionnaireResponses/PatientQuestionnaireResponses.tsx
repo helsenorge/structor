7b765b86d4e13701ed068deca561330135f9231a
Erling Van De Weijer <erling.weijer@gmail.com>
2020-06-25 10:27:06 +0200

    When looking up patients, show their total number of questionnaire responses

---
+++ src/dataexplorer/dataexplorer-application/src/components/PatientQuestionnaireResponses/PatientQuestionnaireResponses.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useFetch from 'utils/hooks/useFetch';
+import { IQuestionnaireResponses } from 'types/IQuestionnaireResponse';
+
+const PatientQuestionnnaireResponses = ({ patientID }: any) => {
+    const { response: questionnaireResponses } = useFetch<
+        IQuestionnaireResponses
+    >('fhir/QuestionnaireResponse?subject=Patient/' + patientID);
+    if (questionnaireResponses) {
+        return (
+            <h1>
+                Pasienten har {questionnaireResponses.total} utfylte skjemaer
+            </h1>
+        );
+    }
+    return <></>;
+};
+
+export default PatientQuestionnnaireResponses;
