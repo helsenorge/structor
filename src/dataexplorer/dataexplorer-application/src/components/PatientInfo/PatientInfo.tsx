a6562b93eb8356ae2a313a70fa6009715c3b318a
Erling Van De Weijer <erling.weijer@gmail.com>
2020-06-25 10:27:06 +0200

    When looking up patients, show their total number of questionnaire responses

--- src/dataexplorer/dataexplorer-application/src/components/PatientInfo/PatientInfo.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/PatientInfo/PatientInfo.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import useFetch from 'utils/hooks/useFetch';
 import { IPatient, IPatientIdentifier } from 'types/IPatient';
+import PatientQuestionnaireResponses from '../PatientQuestionnaireResponses/PatientQuestionnaireResponses';
 
 const PatientInfo = ({ patientID }: any) => {
-
     // The oid signifies that we are searching on social security number
-    const { response } = useFetch<IPatientIdentifier>(
+    const { response: patientData } = useFetch<IPatientIdentifier>(
         'fhir/Patient?identifier=urn:oid:2.16.840.1.113883.2.4.6.3|' +
             patientID,
-
     );
 
-    if (response && response !== undefined) {
+    if (patientData && patientData !== undefined) {
         // Since the search uses social security number, which are
         // unique, response.entry will only contain one element.
-        return displayPatientInfo(response.entry[0].resource);
+        return displayPatientInfo(patientData.entry[0].resource);
     }
     return <div>no information</div>;
 };
@@ -22,11 +21,16 @@ const PatientInfo = ({ patientID }: any) => {
 const displayPatientInfo = (response: IPatient) => {
     return (
         <>
-            <div>navn: {response.name[0].given[0]}</div>
-            <div>kjønn: {response.gender} </div>
-            <div>Fødselsdato: {response.birthDate} </div>
-            <div>Etternavn: {response.name[0].family} </div>
-            <div>ID: {response.id}</div>
+            <div>
+                <div>navn: {response.name[0].given[0]}</div>
+                <div>kjønn: {response.gender} </div>
+                <div>Fødselsdato: {response.birthDate} </div>
+                <div>Etternavn: {response.name[0].family} </div>
+                <div>ID: {response.id}</div>
+            </div>
+            <div>
+                <PatientQuestionnaireResponses patientID={response.id} />
+            </div>
         </>
     );
 };
