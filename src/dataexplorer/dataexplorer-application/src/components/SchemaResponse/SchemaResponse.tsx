ba67940e99b7a726ee0b6e6fc5f5235a802c922b
Frøydis Jørgensen <froydisjo@hotmail.com>
2020-06-25 13:47:36 +0200

    Display title and question from questionnaire

--- src/dataexplorer/dataexplorer-application/src/components/SchemaResponse/SchemaResponse.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/SchemaResponse/SchemaResponse.tsx
@@ -1,78 +1,101 @@
 import React from 'react';
-import { Row } from 'antd';
+import { Row, Col } from 'antd';
 import 'dayjs/locale/nb';
 import Title from 'antd/lib/typography/Title';
 import useFetch from 'utils/hooks/useFetch';
 import dayjs from 'dayjs';
 import { QuestionnaireResponse, Questionnaire } from 'types/fhirTypes/fhir';
 
-const SchemaResponse = async () => {
-    const schemaResponse = await useFetch<QuestionnaireResponse>(
-        'fhir/QuestionnaireResponse/5',
+const SchemaResponse = () => {
+    const questionnaireResponseId = '5';
+    const schemaResponse = useFetch<QuestionnaireResponse>(
+        'fhir/QuestionnaireResponse/' + questionnaireResponseId,
     );
-    const questionnairId = schemaResponse?.questionnaire?.reference?.slice(-2);
+    const questionnaireUrl = schemaResponse.response?.questionnaire?.reference?.substr(
+        schemaResponse.response?.questionnaire?.reference?.indexOf(
+            'Questionnaire/',
+        ),
+    );
     const { response: questionnaire } = useFetch<Questionnaire>(
-        'fhir/Questionnaire/' + questionnairId,
+        'fhir/' + questionnaireUrl,
     );
-    console.log(questionnairId);
-
     console.log(schemaResponse);
     console.log(questionnaire);
     dayjs.locale('nb');
-    const filledInDate = dayjs(schemaResponse?.authored).format(
+    const filledInDate = dayjs(schemaResponse.response?.authored).format(
         'DD/MM/YYYY HH:mm',
     );
-    const firstText = schemaResponse
-        ? schemaResponse.item?.map((i) => i.text)
-        : ['empty'];
-
-    const secondText = schemaResponse
-        ? schemaResponse.item?.map((i) =>
-              i.item?.map((i) => i.text?.toString()),
-          )
-        : ['empty secondText'];
-    console.log(secondText);
-
-    /*     const itemQuestion = schemaResponse?.item?.filter((i) => i.linkId === '3');
-    console.log(
-        itemQuestion && itemQuestion?.length > 0 ? itemQuestion[0] : 'error',
+    const titleQuestionnaire = questionnaire?.item?.flatMap((i) => i.text);
+    const firstQuestion = questionnaire?.item?.map((i) =>
+        i.item?.map((i) => i.text),
     );
+    const secondQuestion = questionnaire?.item?.map((i) =>
+        i.item?.map((i2) => i2.item?.map((i3) => i3.text)),
+    );
+    console.log(secondQuestion);
 
-    const itemAnswer = schemaResponse?.item
-        ?.filter((i) => i.linkId === '3')
-        .flatMap((it) =>
-            it.item?.map((itt) =>
-                itt.answer?.map((a) => a.valueString?.toString()),
-            ),
-        );
-    console.log(itemAnswer && itemAnswer?.length > 0 ? itemAnswer[0] : 'error'); */
     return (
         <>
             {schemaResponse && questionnaire ? (
-                <>
+                <div style={{ marginBottom: '5rem' }}>
                     <Row gutter={40} justify={'center'}>
-                        <Title level={3}>Skjema id:{schemaResponse.id}</Title>
+                        <Title level={3}>
+                            Skjema id:{schemaResponse.response?.id}
+                        </Title>
                     </Row>
                     <Row justify={'center'}>
                         Skjemaet ble fyllt ut: {filledInDate}
                     </Row>
-                    {firstText &&
-                        firstText.map((title, i) => (
-                            <Row justify={'center'} key={i}>
-                                <Title level={4} key={i}>
-                                    {title}
-                                </Title>
-                            </Row>
+                    {questionnaire &&
+                        titleQuestionnaire &&
+                        schemaResponse &&
+                        firstQuestion &&
+                        secondQuestion &&
+                        questionnaire.item?.map((q, i) => (
+                            <div key={i} style={{ marginBottom: '1rem' }}>
+                                <Row justify={'center'}>
+                                    <Title level={4}>
+                                        {titleQuestionnaire[i]}
+                                    </Title>
+                                </Row>
+                                {firstQuestion[i] &&
+                                    firstQuestion[i]?.map(
+                                        (questions, index) => (
+                                            <div key={index}>
+                                                <Row justify={'center'}>
+                                                    <Col
+                                                        style={{
+                                                            fontWeight: 'bold',
+                                                        }}
+                                                    >
+                                                        {questions}
+                                                    </Col>
+                                                </Row>
+                                                {secondQuestion[i] &&
+                                                    secondQuestion[i]?.map(
+                                                        (questions, index) => (
+                                                            <Row
+                                                                justify={
+                                                                    'center'
+                                                                }
+                                                                key={index}
+                                                            >
+                                                                <Col>
+                                                                    {questions}
+                                                                </Col>
+                                                            </Row>
+                                                        ),
+                                                    )}
+                                            </div>
+                                        ),
+                                    )}
+                            </div>
                         ))}
-                    {/*                     {secondText &&
-                        secondText.map((question, i) => (
-                            <Row justify={'center'} key={i}>
-                                <Col key={i}>{question}</Col>
-                            </Row>
-                        ))} */}
-                </>
+                </div>
             ) : (
-                <Title level={4}>Venter...</Title>
+                <Row justify={'center'}>
+                    <Title level={4}>Venter på respons...</Title>
+                </Row>
             )}
         </>
     );
