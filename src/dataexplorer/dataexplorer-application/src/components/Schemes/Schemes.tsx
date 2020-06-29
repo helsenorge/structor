e094ee4775d215e0bd8512266e3ec470ae6bd031
Rebekka Broyn <rebroyn@gmail.com>
2020-06-29 13:18:43 +0200

    commit schemes component

--- src/dataexplorer/dataexplorer-application/src/components/Schemes/Schemes.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/Schemes/Schemes.tsx
@@ -1,63 +1,33 @@
 import React from 'react';
-import { Collapse, Row, Col, Popover, Button, Card } from 'antd';
+import { Collapse, Row, Popover, Button } from 'antd';
 import './Schemes.style.scss';
 
-const Schemes = () => {
+type Schemetype = {
+    title: string;
+    sections: {
+        sect: string;
+        key: string;
+        qa: {
+            q: string;
+            a: string[];
+            alt: string[];
+            subarray: {
+                subq: string;
+                suba: string[];
+                subalt: string[];
+            }[];
+        }[];
+    }[];
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const Schemes = ({ title, sections }: Schemetype) => {
     const { Panel } = Collapse;
-
-    const title = 'Sykdomsskjema';
-
-    const sections = [
-        {
-            sect: 'Symptomer',
-            key: '1',
-            qa: [
-                {
-                    q:
-                        'Har du i løpet av de siste syv dagene fått symptomer som kan skyldes koronavirus? ',
-                    a: ['ja'],
-
-                    b: ['ja', 'nei'],
-                },
-                {
-                    q: 'Hvilke symptomer har du hatt?',
-                    a: [
-                        'Tett eller rennende nese',
-                        'Hoste',
-                        'Tap av smaks- eller luktesans',
-                        'Sår hals',
-                    ],
-                    b: ['Hoste', 'Feber', 'Sår hals', 'Andre'],
-                },
-                { q: 'spørsmål3', a: ['svar3'], b: [] },
-            ],
-        },
-        {
-            sect: 'Sykdom',
-            key: '2',
-            qa: [
-                { q: 'spørsmål4: ', a: ['svar4'], b: [] },
-                { q: 'spørsmål5', a: ['svar5'], b: ['heu', 'sei', 'meg'] },
-                { q: 'spørsmål6', a: ['svar6'], b: [] },
-            ],
-        },
-        {
-            sect: 'Korona',
-            key: '3',
-            qa: [
-                { q: 'spørsmål7: ', a: ['svar7'], b: [] },
-                { q: 'spørsmål8', a: ['svar8'], b: [] },
-                { q: 'spørsmål9', a: ['svar9'], b: ['yo', 'lo', 'no'] },
-                { q: 'spørsmål10: ', a: ['svar10'], b: [] },
-                { q: 'spørsmål11', a: ['svar11'], b: [] },
-                { q: 'spørsmål12', a: ['svar12'], b: [] },
-            ],
-        },
-    ];
 
     function setContent(data: string[]) {
         return data.map((line) => <li key={line}>{line}</li>);
     }
+
     return (
         <>
             <Row justify="center">
@@ -74,25 +44,17 @@ const Schemes = () => {
                                 key={key}
                                 className="site-collapse-custom-panel"
                             >
-                                {qa.map(({ q, a, b }) => (
+                                {qa.map(({ q, a, alt, subarray }) => (
                                     <>
                                         <div className="boarder">
                                             <br></br>
                                             <p className="questions"> {q}</p>
-                                            <p className="bold">Svar:</p>
-                                            {a.map((item) => (
-                                                <p
-                                                    className="answers"
-                                                    key={item}
-                                                >
-                                                    {item}
-                                                </p>
-                                            ))}
-                                            {b.length > 0 ? (
+                                            <p className="inline">Svar: </p>
+                                            {alt.length > 0 ? (
                                                 <Popover
                                                     placement="rightTop"
                                                     trigger="click"
-                                                    content={setContent(b)}
+                                                    content={setContent(alt)}
                                                 >
                                                     <Button
                                                         className="nopadding"
@@ -102,6 +64,49 @@ const Schemes = () => {
                                                     </Button>
                                                 </Popover>
                                             ) : null}
+                                            {a.map((item) => (
+                                                <p
+                                                    className="answers"
+                                                    key={item}
+                                                >
+                                                    {item}
+                                                </p>
+                                            ))}
+                                            {subarray.map(
+                                                ({ subalt, subq, suba }) =>
+                                                    suba.length > 0 ? (
+                                                        <>
+                                                            <br></br>
+                                                            <p className="questions">
+                                                                {subq}
+                                                            </p>
+                                                            <p className="inline">
+                                                                Svar:{' '}
+                                                            </p>
+                                                            {subalt.length >
+                                                            0 ? (
+                                                                <Popover
+                                                                    placement="rightTop"
+                                                                    trigger="click"
+                                                                    content={setContent(
+                                                                        subalt,
+                                                                    )}
+                                                                >
+                                                                    <Button
+                                                                        className="nopadding"
+                                                                        type="link"
+                                                                    >
+                                                                        (Vis
+                                                                        alternativer)
+                                                                    </Button>
+                                                                </Popover>
+                                                            ) : null}
+                                                            <p className="answers">
+                                                                {suba}
+                                                            </p>
+                                                        </>
+                                                    ) : null,
+                                            )}
                                         </div>
                                     </>
                                 ))}
