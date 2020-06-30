66826b8cb82749addd821e7548b4723b6593cbdb
EnvyPhD <twiller3@hotmail.com>
2020-06-30 15:48:13 +0200

    Moved breadcrumbs, general styling, removed duplicate from navigation (??)

--- src/dataexplorer/dataexplorer-application/src/components/Navigation/Navigation.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/Navigation/Navigation.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Switch, Route, withRouter, Link } from 'react-router-dom';
-import Breadcrumbs from './Breadcrumbs/Breadcrumbs';
-import MyPatients from '../PatientInfo/MyPatients';
-import PatientInfo from '../PatientInfo/PatientInfo';
-import 'antd/dist/antd.css';
 import { Layout, Avatar } from 'antd';
+import Breadcrumbs from './Breadcrumbs/Breadcrumbs';
+import 'antd/dist/antd.css';
+import MyPatients from '../PatientInfo/MyPatients';
+import { Switch, Route, withRouter, Link } from 'react-router-dom';
 import { UserOutlined } from '@ant-design/icons';
+import PatientInfo from '../PatientInfo/PatientInfo';
 import './Navigation.style.scss';
 import {History } from 'history'
 
@@ -17,46 +17,42 @@ const Navigation = (props: { history: History }) => {
     return (
         <>
             <Layout>
-                <Layout>
-                    <Header className="site-layout-sub-header-background">
-                        <Link to="/">
-                            <h1>Datautforskeren</h1>
-                        </Link>
-                        <Breadcrumbs
-                            name={name}
-                            schemaNumber={schemaNumber}
+                <Header className="site-layout-sub-header-background">
+                    <Link to="/">
+                        <h1>Datautforskeren</h1>
+                    </Link>
+
+                    <div className="avatar-container">
+                        <Avatar
+                            className="header-avatar"
+                            shape="square"
+                            size="large"
+                            icon={<UserOutlined />}
                         />
-                        <div className="avatar-container">
-                            <Avatar
-                                className="header-avatar"
-                                shape="square"
-                                size="large"
-                                icon={<UserOutlined />}
+                        <h1>Dr. Gregory House</h1>
+                    </div>
+                </Header>
+                <div className="breadcrumb-header">
+                    <Breadcrumbs name={name} schemaNumber={schemaNumber} />
+                </div>
+                <Content>
+                    <div className="content">
+                        <Switch>
+                            <Route exact path="/" component={MyPatients} />
+                            <Route
+                                exact
+                                path="/Pasient"
+                                render={() => (
+                                    <PatientInfo
+                                        setName={setName}
+                                        setSchema={setSchema}
+                                        patientID={props.history.location.state}
+                                    />
+                                )}
                             />
-                            <h1>Dr. Gregory House</h1>
-                        </div>
-                    </Header>
-                    <Content>
-                        <div className="content">
-                            <Switch>
-                                <Route exact path="/" component={MyPatients} />
-                                <Route
-                                    exact
-                                    path="/Pasient"
-                                    render={() => (
-                                        <PatientInfo
-                                            setName={setName}
-                                            setSchema={setSchema}
-                                            patientID={
-                                                props.history.location.state
-                                            }
-                                        />
-                                    )}
-                                />
-                            </Switch>
-                        </div>
-                    </Content>
-                </Layout>
+                        </Switch>
+                    </div>
+                </Content>
             </Layout>
         </>
     );
