8b09d3ad0a0917e043f8eee7358f8faa864d6fac
EnvyPhD <twiller3@hotmail.com>
2020-06-30 09:23:37 +0200

    Remove: serviceworker.

--- src/dataexplorer/dataexplorer-application/src/index.tsx
+++ src/dataexplorer/dataexplorer-application/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
@@ -13,8 +12,3 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
