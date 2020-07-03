d7c69ad8940a1179ab7c383e822486430db80fd4
EnvyPhD <twiller3@hotmail.com>
2020-07-03 15:37:25 +0200

    Fix: Fixed bug on undefined birthday. Fixed gendercheck

---
+++ src/dataexplorer/dataexplorer-application/src/components/PatientQuestionnaireResponses/PatientQuestionnaireResponses.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useFetch from 'utils/hooks/useFetch';
+import { IQuestionnaireResponse } from 'types/IQuestionnaireResponse';
+
+const PatientQuestionnnaireResponses = (props: { patientID: string }) => {
+    const { response: patientQuestionnaireResponses } = useFetch<
+        IQuestionnaireResponse
+    >('fhir/QuestionnaireResponse?subject=Patient/' + props.patientID);
+    if (patientQuestionnaireResponses) {
+        return (
+            <h1>
+                Pasienten har
+                {patientQuestionnaireResponses?.entry?.length}
+                utfylte skjemaer
+            </h1>
+        );
+    }
+    return <></>;
+};
+
+export default PatientQuestionnnaireResponses;
