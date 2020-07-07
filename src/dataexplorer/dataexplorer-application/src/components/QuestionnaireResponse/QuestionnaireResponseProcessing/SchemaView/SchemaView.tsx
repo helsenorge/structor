583f0babc80e09b26427b5d9520cd7856c5320b5
Rebekka Broyn <rebroyn@gmail.com>
2020-07-07 14:35:11 +0200

    fixed conflicts

--- src/dataexplorer/dataexplorer-application/src/components/QuestionnaireResponse/QuestionnaireResponseProcessing/SchemaView/SchemaView.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/QuestionnaireResponse/QuestionnaireResponseProcessing/SchemaView/SchemaView.tsx
@@ -10,7 +10,7 @@ import dayjs, { Dayjs } from 'dayjs';
 import QuestionsAndAnswersDisplay from './QuestionsAndAnswersDisplay/QuestionsAndAnswersDisplay';
 import SubQuestionsAndAnswersDisplay from './SubQuestionsAndAnswersDisplay/SubQuestionsAndAnswersDisplay';
 
-export interface SchemesProps {
+export interface SchemaViewProps {
     questions: IQuestion[];
     answers: IAnswer[];
     questionnaireResource: fhir.ValueSet[];
@@ -18,7 +18,7 @@ export interface SchemesProps {
     title: string;
 }
 
-const SchemaView = (props: SchemesProps) => {
+const SchemaView = (props: SchemaViewProps) => {
     dayjs.locale('nb');
     const [qAndA, setQAndA] = useState<IQuestionAndAnswer[]>([]);
     const [qAndAIDs, setQAndAIDs] = useState<string[]>([]);
