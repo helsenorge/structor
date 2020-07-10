cb24c81e7a3c7d4bf9fab7939901f7565c6dea7d
Frøydis Jørgensen <froydisjo@hotmail.com>
2020-07-10 15:07:07 +0200

    Show descrition button if description exists

--- src/dataexplorer/dataexplorer-application/.eslintrc.js
+++ src/dataexplorer/dataexplorer-application/.eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-	    '@typescript-eslint/no-empty-function': 'off'
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
     },
 };
