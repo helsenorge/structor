daa012448f51fb0ad6d6cf5d87983de67415331c
Erling Van De Weijer <erling.weijer@gmail.com>
2020-07-14 10:19:38 +0200

    Refractor breadcrumbcontext to patientcontext, as it will now also contain IPatientIdentifier object

--- src/dataexplorer/dataexplorer-application/src/components/Patient/PatientContext.ts
+++ src/dataexplorer/dataexplorer-application/src/components/Patient/PatientContext.ts
@@ -1,20 +1,25 @@
 import { createContext, Dispatch, SetStateAction } from 'react';
+import { IPatientIdentifier } from 'types/IPatient';
 
-interface IBreadcrumbContextProps {
+interface IPatientContext {
     name: string;
     setName: Dispatch<SetStateAction<string>>;
     patientId: string;
     setPatientId: Dispatch<SetStateAction<string>>;
     schemaNumber: string;
     setSchemanumber: Dispatch<SetStateAction<string>>;
+    patient: IPatientIdentifier;
+    setPatient: Dispatch<SetStateAction<IPatientIdentifier>>;
 }
-export const BreadcrumbContext = createContext<IBreadcrumbContextProps>({
+export const PatientContext = createContext<IPatientContext>({
     name: '',
     setName: () => {},
     patientId: '',
     setPatientId: () => {},
     schemaNumber: '',
     setSchemanumber: () => {},
+    patient: {} as IPatientIdentifier,
+    setPatient: () => {},
 });
 
-export const { Provider } = BreadcrumbContext;
+export const { Provider } = PatientContext;
