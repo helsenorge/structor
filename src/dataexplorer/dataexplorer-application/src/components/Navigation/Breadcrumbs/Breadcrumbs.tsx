0a56a4863d2fe523fcc90a2d1afbeaf979414044
Erling Van De Weijer <erling.weijer@gmail.com>
2020-07-15 16:11:54 +0200

    Make relevant breadcrumb bold

--- src/dataexplorer/dataexplorer-application/src/components/Navigation/Breadcrumbs/Breadcrumbs.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/Navigation/Breadcrumbs/Breadcrumbs.tsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import Breadcrumb from 'antd/lib/breadcrumb';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Breadcrumbs.style.scss';
 import { GlobalContext } from 'context/GlobalContext';
 
 const Breadcrumbs = () => {
     const { name, schemaNumber, setPatientId, comparableSchemaNumbers } = useContext(GlobalContext);
+    const location = useLocation();
+    console.log(location.pathname);
     return (
         <>
             <div className="breadcrumbs-container">
@@ -20,22 +22,34 @@ const Breadcrumbs = () => {
                     {name !== '' && (
                         <Breadcrumb.Item>
                             <Link to="/pasient">
-                                <span id="breadcrumb-name" className="breadcrumbs-item">
-                                    {name}
-                                </span>
+                                {location.pathname === '/pasient' ? (
+                                    <span id="breadcrumb-name-focus" className="breadcrumbs-item">
+                                        {name}
+                                    </span>
+                                ) : (
+                                    <span id="breadcrumb-name" className="breadcrumbs-item">
+                                        {name}
+                                    </span>
+                                )}
                             </Link>
                         </Breadcrumb.Item>
                     )}
                     {schemaNumber !== '' && (
                         <Breadcrumb.Item>
-                            <span id="breadcrumb-schema" className="breadcrumbs-item">
-                                Skjema {schemaNumber}
-                            </span>
+                            {location.pathname === '/pasient/skjema' ? (
+                                <span id="breadcrumb-schema-focus" className="breadcrumbs-item">
+                                    Skjema {schemaNumber}
+                                </span>
+                            ) : (
+                                <span id="breadcrumb-schema" className="breadcrumbs-item">
+                                    Skjema {schemaNumber}
+                                </span>
+                            )}
                         </Breadcrumb.Item>
                     )}
                     {comparableSchemaNumbers.length !== 0 && (
                         <Breadcrumb.Item>
-                            <span className="breadcrumbs-schema">
+                            <span id="breadcrumb-schema-focus" className="breadcrumbs-item">
                                 Sammenligner skjemaene: {comparableSchemaNumbers[0]} og {comparableSchemaNumbers[1]}
                             </span>
                         </Breadcrumb.Item>
