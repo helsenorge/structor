5f2e1d7f495d8d4275105617ec1fe836e6eee9e3
René Leonard Aasen <reneleonardaasen@gmail.com>
2020-12-10 14:48:43 +0100

    validating - Integer visual

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -240,12 +240,75 @@ const Question = (props: QuestionProps): JSX.Element => {
                 </div>*/}
                 {respondType(props.item.type)}
             </div>
+
             <div className="question-addons">
                 <Accordion title="Legg til validering">
-                    <p>
-                        Hvis relevansen for dette spørsmålet er avhgengig av svaret på et tidligere spørsmål, velger du
-                        dette her.{' '}
-                    </p>
+                    {/* CHECK FOR TYPE */}
+                    {props.item.type === IQuestionnaireItemType.integer && (
+                        <>
+                            <div className="validating-help-title">
+                                <p>Veiledende tekst</p>
+                            </div>
+
+                            <div className="allow-decimal">
+                                <input
+                                    type="checkbox"
+                                    onChange={() => {
+                                        // TODO
+                                    }}
+                                />
+                                <span> Tillat desimaltall</span>
+                            </div>
+
+                            <div className="form-field half">
+                                <label className="#">Svaret skal være:</label>
+                                <Select
+                                    options={[
+                                        { display: 'Velg kriterie', code: '0' },
+                                        { display: 'Større enn', code: '1' },
+                                        { display: 'Mindre enn', code: '2' },
+                                    ]}
+                                    onChange={() => {
+                                        //TODO!
+                                    }}
+                                ></Select>
+                            </div>
+
+                            {/* SHOW WHEN SELECED A VALUE */}
+                            <div className="form-field half">
+                                <label className="#">Skriv inn ett tall:</label>
+                                <input
+                                    type="input"
+                                    placeholder="5"
+                                    onChange={() => {
+                                        //TODO
+                                    }}
+                                ></input>
+                            </div>
+
+                            <div className="form-field">
+                                <a
+                                    href="#"
+                                    onClick={() => {
+                                        //TODO
+                                    }}
+                                >
+                                    + Legg til kriterie
+                                </a>
+                            </div>
+
+                            <div className="form-field custom-input-error-message">
+                                <label className="#">Legg til egendefinert feilmelding:</label>
+                                <input
+                                    type="input"
+                                    placeholder="feilmelding"
+                                    onChange={() => {
+                                        //TODO
+                                    }}
+                                ></input>
+                            </div>
+                        </>
+                    )}
                 </Accordion>
                 <Accordion title="Legg til betinget visning">
                     <p>
