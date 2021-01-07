9be692206f1868443b38bc8c0665932d3f7dafea
Jørgen H. Brække <jorgen.brakke@capgemini.com>
2021-01-07 14:44:01 +0100

    working version

--- src/components/AnchorMenu/AnchorMenu.tsx
+++ src/components/AnchorMenu/AnchorMenu.tsx
@@ -6,15 +6,16 @@ const AnchorMenu = (): JSX.Element => {
     const initList = [
         {
             id: '1',
-            text: 'hi',
+            text: 'Her kan det være et godt spørmsål',
         },
         {
             id: '2',
-            text: 'over',
+            text: 'Litt lenger spørsmål',
         },
         {
-            id: '3',
-            text: 'there',
+            id: '4',
+            text:
+                'Du har tatt en test som ikke påviser koronavirus. Du trenger vanligvis ikke å teste deg på nytt dersom du akkurat har fått et svar som ikke påviser koronavirus?',
         },
     ];
 
@@ -33,21 +34,48 @@ const AnchorMenu = (): JSX.Element => {
         setList(alteredArray);
     };
 
+    const grid = 8;
+
+    const getListStyle = (isDraggingOver: boolean) => ({
+        background: isDraggingOver ? 'lightblue' : 'lightgrey',
+        padding: grid,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+        // some basic styles to make the items look a bit nicer
+        userSelect: 'none',
+        padding: grid * 2,
+        margin: `0 0 ${grid}px 0`,
+
+        // change background colour if dragging
+        background: isDragging ? 'lightgreen' : 'grey',
+
+        // styles we need to apply on draggables
+        ...draggableStyle,
+    });
+
     return (
         <div className="anchor-menu">
-            <h1>Anker</h1>
+            <p className="align-header">Oversikt</p>
+
             <DragDropContext onDragEnd={handleChange}>
                 <Droppable droppableId="droppable">
                     {(provided, snapshot) => (
-                        <div {...provided.droppableProps} ref={provided.innerRef}>
+                        <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            style={getListStyle(snapshot.isDraggingOver)}
+                        >
                             {list.map((item, index) => {
                                 return (
-                                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                                    <Draggable key={index} draggableId={index.toString()} index={index}>
                                         {(provided, snapshot) => (
                                             <div
                                                 ref={provided.innerRef}
                                                 {...provided.draggableProps}
                                                 {...provided.dragHandleProps}
+                                                style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
                                             >
                                                 {item.text}
                                             </div>
@@ -55,6 +83,7 @@ const AnchorMenu = (): JSX.Element => {
                                     </Draggable>
                                 );
                             })}
+                            {provided.placeholder}
                         </div>
                     )}
                 </Droppable>
