d276013f15ec9a3d145e534a183e5f2189657649
Geir André Løken <geir-andre.loken@capgemini.com>
2021-03-17 13:23:13 +0100

    Revert "Testing sonar exclusion"
    This reverts commit 83696f17087e37f7069605c6281ba1347af440d1.

--- src/types/IQuestionnareItemType.ts
+++ src/types/IQuestionnareItemType.ts
@@ -64,10 +64,10 @@ export type IEnableWhen = {
 };
 
 export enum IExtentionType {
-    validationtext = 'https://ehelse.no/fhir/StructureDefinition/validationtext',
+    validationtext = 'http://ehelse.no/fhir/StructureDefinition/validationtext',
     minValue = 'http://hl7.org/fhir/StructureDefinition/minValue',
     maxValue = 'http://hl7.org/fhir/StructureDefinition/maxValue',
-    maxSize = 'http://hl7.org/fhir/StructureDefinition/maxSize', // NOSONAR
+    maxSize = 'http://hl7.org/fhir/StructureDefinition/maxSize',
     regEx = 'http://hl7.org/fhir/StructureDefinition/regex',
     maxDecimalPlaces = 'http://hl7.org/fhir/StructureDefinition/maxDecimalPlaces',
     markdown = 'http://hl7.org/fhir/StructureDefinition/rendering-markdown',
