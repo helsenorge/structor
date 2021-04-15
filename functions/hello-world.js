7fb01a860317b39648185eda6cb6f8df243d1169
Jørgen H. Brække <jorgen.brakke@capgemini.com>
2021-04-15 16:05:39 +0200

    netlify test

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const axios = require('axios');
+
 exports.handler = async function () {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Hello World' }),
+        body: JSON.stringify({ data: response.data, key: process.env.KEY || 'NO-KEY :(' }),
     };
 };
