f039f95d6854187249f6a072c597e5d97c664fbe
Geir André Løken <geir-andre.loken@capgemini.com>
2021-04-28 14:17:33 +0200

    Add react-hooks plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,12 @@ module.exports = {
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors.
         // Make sure this is always the last configuration in the extends array.
     ],
+    plugins: ['react-hooks'],
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         '@typescript-eslint/no-empty-interface': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
     },
 };
