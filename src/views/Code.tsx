0ad092574a526f65270ec339ac3edaf739e06b47
Geir André Løken <geir-andre.loken@capgemini.com>
2021-04-29 12:12:51 +0200

    Use recommended eslint rules for react-hooks plugin

--- src/views/Code.tsx
+++ src/views/Code.tsx
@@ -8,31 +8,33 @@ const Code = (): JSX.Element => {
 
     const history = useHistory();
 
-    async function getToken() {
-        const urlParams = window.location.search;
-        const code_verifier = localStorage.getItem('code_verifier');
+    useEffect(() => {
+        async function getToken() {
+            const urlParams = window.location.search;
+            const code_verifier = localStorage.getItem('code_verifier');
 
-        if (urlParams && code_verifier) {
-            try {
-                let response = await fetch(`.netlify/functions/get-token${urlParams}&code_verifier=${code_verifier}`);
-                if (response.ok) {
-                    response = await response.json();
-                    localStorage.setItem('code_verifier', '');
-                    sessionStorage.setItem('profile', JSON.stringify(response));
-                    history.replace('/');
-                } else {
-                    throw new Error(`Server status: ${response.status}`);
+            if (urlParams && code_verifier) {
+                try {
+                    let response = await fetch(
+                        `.netlify/functions/get-token${urlParams}&code_verifier=${code_verifier}`,
+                    );
+                    if (response.ok) {
+                        response = await response.json();
+                        localStorage.setItem('code_verifier', '');
+                        sessionStorage.setItem('profile', JSON.stringify(response));
+                        history.replace('/');
+                    } else {
+                        throw new Error(`Server status: ${response.status}`);
+                    }
+                } catch (err) {
+                    console.error('Error!', err);
+                    setError(() => `Noe gikk galt, kontakt admin.`);
                 }
-            } catch (err) {
-                console.error('Error!', err);
-                setError(() => `Noe gikk galt, kontakt admin.`);
             }
         }
-    }
 
-    useEffect(() => {
         getToken();
-    }, []);
+    }, [history]);
 
     return (
         <div className="login">
