90ae6ecaad4a9f1d8e4247e8d221d48d2c225045
Martin Gunnerud <martin.gunnerud@capgemini.com>
2021-08-16 14:59:32 +0200

    clean up examples + code used to create minified preview code (requires helsenorge npm-key to build new preview version)

--- examples/form-filler/webpack.config.js
+++ examples/form-filler/webpack.config.js
@@ -2,7 +2,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
-    const isDevBuild = (env && env.development);
+    const isDevBuild = env && env.development;
 
     return {
         entry: './src/index.jsx',
@@ -10,44 +10,57 @@ module.exports = (env) => {
             port: 3000,
             historyApiFallback: true,
         },
-        devtool: "source-map",
+        devtool: 'source-map',
         module: {
             rules: [
                 {
                     test: /\.(js|jsx)$/,
                     exclude: /node_modules/,
                     use: {
-                        loader: "babel-loader"
-                    }
+                        loader: 'babel-loader',
+                    },
                 },
                 {
                     test: /\.(s*)css$/,
-                    use: ['style-loader', 'css-loader', 'sass-loader']
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                // Run `postcss-loader` on each CSS `@import`, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+                                // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+                                importLoaders: 1,
+                                // Automatically enable css modules for files satisfying `/\.module\.\w+$/i` RegExp.
+                                modules: { auto: true },
+                            },
+                        },
+                        'sass-loader',
+                    ],
                 },
                 {
                     test: /\.(woff|woff2|eot|ttf|otf|svg|gif)$/,
-                    loader: "file-loader"
-                }
-            ]
+                    loader: 'file-loader',
+                },
+            ],
         },
         node: {
-            module: "empty", 
-            net: "empty", 
-            fs: "empty"
+            module: 'empty',
+            net: 'empty',
+            fs: 'empty',
         },
         resolve: {
-            extensions: ['.jsx', '.js']
+            extensions: ['.jsx', '.js'],
         },
 
         output: {
             filename: 'bundle.js',
             path: isDevBuild ? path.resolve(__dirname, 'dist') : path.resolve(__dirname, 'wwwroot/dist'),
-            publicPath: isDevBuild ? '/' : 'dist/'
+            publicPath: isDevBuild ? '/' : 'dist/',
         },
         plugins: [
             new HtmlWebpackPlugin({
-                title: 'Skjemautfyller-eksempel'
-            })
-        ]
+                title: 'Skjemautfyller-eksempel',
+            }),
+        ],
     };
 };
