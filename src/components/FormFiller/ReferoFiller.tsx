329cb0eda2f78ef4ba867f2446dda9df92c2b21a
anagelpe <andreas.nagel-petersen@nhn.no>
2022-06-20 13:53:53 +0200

    Installed refero and created new file for questionaire preview

---
+++ src/components/FormFiller/ReferoFiller.tsx
@@ -0,0 +1,37 @@
+import { useContext, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { TreeContext } from '../../store/treeStore/treeStore';
+import { generateQuestionnaireForPreview } from '../../helpers/generateQuestionnaire';
+import { getLanguagesInUse, INITIAL_LANGUAGE } from '../../helpers/LanguageHelper';
+import { ReferoContainer } from '@helsenorge/refero/components';
+
+type Props = {
+    showFormFiller: () => void;
+    language?: string;
+};
+
+const ReferoFiller = ({ showFormFiller, language }: Props): JSX.Element => {
+    const { t } = useTranslation();
+    const { state } = useContext(TreeContext);
+    const [selectedLanguage, setSelectedLanguage] = useState<string | undefined>(
+        language || state.qMetadata.language || INITIAL_LANGUAGE.code,
+    );
+    const [selectedGender, setSelectedGender] = useState<string>('');
+    const [selectedAge, setSelectedAge] = useState<string>('');
+    const [selectedReceiverEndpoint, setSelectedReceiverEndpoint] = useState<string>('');
+    const languages = getLanguagesInUse(state);
+
+    return (
+        <ReferoContainer
+            questionnaire={generateQuestionnaireForPreview(state, selectedLanguage, selectedGender, selectedAge)}
+            onCancel={showFormFiller}
+            onSave={console.log('save')}
+            onSubmit={console.log('submit')}
+            loginButton={<button>{'hei'}</button>}
+            authorized={false}
+        />
+    );
+};
+
+export default ReferoFiller;
