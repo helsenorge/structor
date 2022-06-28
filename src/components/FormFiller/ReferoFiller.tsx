187081c4cfdeacb9a872687031c3e5bff56d7137
anagelpe <andreas.nagel-petersen@nhn.no>
2022-06-28 12:43:13 +0200

    Installed redux-thunk and added navbar in ReferoFiller

--- src/components/FormFiller/ReferoFiller.tsx
+++ src/components/FormFiller/ReferoFiller.tsx
@@ -10,6 +10,9 @@ import { Store, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from '@helsenorge/refero/reducers';
+import IconBtn from '../IconBtn/IconBtn';
+import Select from '../Select/Select';
+import { isItemControlReceiverComponent } from '../../helpers/itemControl';
 
 type Props = {
     showFormFiller: () => void;
@@ -30,17 +33,100 @@ const ReferoFiller = ({ showFormFiller, language }: Props): JSX.Element => {
     const [selectedReceiverEndpoint, setSelectedReceiverEndpoint] = useState<string>('');
     const languages = getLanguagesInUse(state);
 
+    function hasReceiverComponent(): boolean {
+        return (
+            Object.keys(state.qItems).filter((linkId) => isItemControlReceiverComponent(state.qItems[linkId])).length >
+            0
+        );
+    }
+
     return (
         <Provider store={store}>
-            <ReferoContainer
-                store={store}
-                questionnaire={generateQuestionnaireForPreview(state, selectedLanguage, selectedGender, selectedAge)}
-                onCancel={showFormFiller}
-                onSave={console.log('save')}
-                onSubmit={console.log('submit')}
-                loginButton={<button>{'hei'}</button>}
-                authorized={false}
-            />
+            <div className="overlay">
+                <div className="iframe-div">
+                    <div className="title align-everything">
+                        <IconBtn type="x" title={t('Close')} onClick={showFormFiller} />
+                        <h1>{t('Preview')}</h1>
+                        <div className="pull-right">
+                            {hasReceiverComponent() && (
+                                <input
+                                    style={{ padding: '0 10px', border: 0, width: 250 }}
+                                    placeholder={t('Recipient component EndpointId')}
+                                    onBlur={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                        setSelectedReceiverEndpoint(event.target.value);
+                                    }}
+                                />
+                            )}
+                            <Select
+                                value={selectedGender}
+                                options={[
+                                    {
+                                        code: '',
+                                        display: t('Gender'),
+                                    },
+                                    {
+                                        code: 'Kvinne',
+                                        display: t('Female'),
+                                    },
+                                    {
+                                        code: 'Mann',
+                                        display: t('Male'),
+                                    },
+                                    {
+                                        code: 'Ukjent',
+                                        display: t('Unknown'),
+                                    },
+                                ]}
+                                onChange={(e) => {
+                                    setSelectedGender(e.target.value);
+                                }}
+                                compact={true}
+                            />
+                            <Select
+                                value={selectedAge}
+                                options={[
+                                    {
+                                        code: '',
+                                        display: t('Age'),
+                                    },
+                                    ...Array.from(Array(120), (_x, index) => {
+                                        return {
+                                            code: index.toString(),
+                                            display: index.toString(),
+                                        };
+                                    }),
+                                ]}
+                                onChange={(e) => {
+                                    setSelectedAge(e.target.value);
+                                }}
+                                compact={true}
+                            />
+                            <Select
+                                value={selectedLanguage}
+                                options={languages}
+                                onChange={(e) => {
+                                    setSelectedLanguage(e.target.value);
+                                }}
+                                compact={true}
+                            />
+                        </div>
+                    </div>
+                    <ReferoContainer
+                        store={store}
+                        questionnaire={generateQuestionnaireForPreview(
+                            state,
+                            selectedLanguage,
+                            selectedGender,
+                            selectedAge,
+                        )}
+                        onCancel={showFormFiller}
+                        onSave={console.log('save')}
+                        onSubmit={console.log('submit')}
+                        loginButton={<button>{'hei'}</button>}
+                        authorized={false}
+                    />
+                </div>
+            </div>
         </Provider>
     );
 };
