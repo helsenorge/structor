531cd2e9b5f4b9b1df8c10ee7be52b9573461866
anagelpe <andreas.nagel-petersen@nhn.no>
2022-09-06 10:40:06 +0200

    Removed iframe folder and old FormFiller component

--- examples/form-filler/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './app';
-
-let root = document.createElement('div');
-document.body.appendChild(root);
-
-ReactDOM.render(<App />, root);
-
--- examples/form-filler/src/root/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import form from '@helsenorge/skjemautfyller/reducers/form';
-
-const rootReducer = combineReducers({
-    skjemautfyller: combineReducers({ form: form }),
-});
-
-export default rootReducer;
