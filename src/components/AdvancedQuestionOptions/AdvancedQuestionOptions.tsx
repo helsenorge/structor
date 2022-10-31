6b611cd555672b90d473e728f8f2c5bfbf6e76ff
Arezoo Ghalichi <arezoo.ghalichi@nhn.no>
2022-10-31 07:12:02 +0100

    Enable summary group

--- src/components/AdvancedQuestionOptions/AdvancedQuestionOptions.tsx
+++ src/components/AdvancedQuestionOptions/AdvancedQuestionOptions.tsx
@@ -347,7 +347,7 @@ const AdvancedQuestionOptions = ({ item, parentArray }: AdvancedQuestionOptionsP
                                 setItemControlExtension(item, ItemControlType.summaryContainer, dispatch);
                             }}
                             value={hasSummaryContainerExtension}
-                            label={t('Enable summary')}
+                            label={t('Enable summary group')}
                         />
                     </FormField>
                 </div>
