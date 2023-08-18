9ba8d157f9aed918d76261b9cc4de94c03ca81f9
anagelpe <andreas.nagel-petersen@nhn.no>
2023-08-18 10:44:57 +0200

    Removed more unused imports

--- src/components/AnswerOption/AnswerOption.tsx
+++ src/components/AnswerOption/AnswerOption.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DraggableProvidedDragHandleProps } from 'react-beautiful-dnd';
 import { QuestionnaireItem, QuestionnaireItemAnswerOption } from '../../types/fhir';
