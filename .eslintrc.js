0f41becdb3121cdd0cc68a9f183e82eea4b9f356
anagelpe <andreas.nagel-petersen@nhn.no>
2023-09-01 11:25:57 +0200

    Added rule to make prettier errors into warnings instead

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         '@typescript-eslint/no-empty-interface': 'off',
+        'prettier/prettier': 'warn',
     },
 };
