6cb4e2748f3892871a686a833cb77d4962d567ab
Firmansyah Otoluwa <pirmansah.otoluwa@gmail.com>
2023-11-19 20:28:27 +0700

    RTE license compatibility

--- src/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { EditorState, Modifier, RichUtils, convertFromRaw, convertToRaw } from 'draft-js';
+import { draftToMarkdown, markdownToDraft } from 'markdown-draft-js';
 import React, { useState } from 'react';
-// @ts-ignore
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-// @ts-ignore
-import Editor from '@helsenorge/ckeditor5-build-markdown';
-
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './MarkdownEditor.css';
-
 interface MarkdownEditorProps {
     data: string;
     onBlur?: (data: string) => void;
@@ -15,45 +12,113 @@ interface MarkdownEditorProps {
 }
 
 const MarkdownEditor = (props: MarkdownEditorProps): JSX.Element => {
-    const [value, setValue] = useState<string>(props.data);
-    const handleChange = (event: Event, editor: Editor) => {
-        setValue(editor.getData());
-    };
-    const handleBlur = (event: Event, editor: Editor) => {
+    const rawData = markdownToDraft(props.data);
+    const contentState = convertFromRaw(rawData);
+
+    const [editorState, setEditorState] = useState(() => EditorState.createWithContent(contentState));
+
+    const content = editorState.getCurrentContent();
+
+    const handleBlur = () => {
+        const rawObject = convertToRaw(content);
+        const markdownString = draftToMarkdown(rawObject);
+
         if (props.onBlur) {
-            props.onBlur(editor.getData());
+            props.onBlur(markdownString);
         }
     };
 
-    const editorConfiguration = {
-        toolbar: [
-            'heading',
-            '|',
-            'bold',
-            'italic',
-            'link',
-            'bulletedList',
-            'numberedList',
-            '|',
-            'blockQuote',
-            '|',
-            'undo',
-            'redo',
-        ],
-        language: 'no-nb',
-        placeholder: props.placeholder || '',
+    const removeSelectedBlocksStyle = (editorState: EditorState) => {
+        const newContentState = RichUtils.tryToRemoveBlockStyle(editorState);
+        if (newContentState) {
+            return EditorState.push(editorState, newContentState, 'change-block-type');
+        }
+        return editorState;
+    };
+
+    const getResetEditorState = (editorState: EditorState) => {
+        const blocks = editorState.getCurrentContent().getBlockMap().toList();
+        const updatedSelection = editorState.getSelection().merge({
+            anchorKey: blocks.first().get('key'),
+            anchorOffset: 0,
+            focusKey: blocks.last().get('key'),
+            focusOffset: blocks.last().getLength(),
+        });
+        const newContentState = Modifier.removeRange(editorState.getCurrentContent(), updatedSelection, 'forward');
+
+        const newState = EditorState.push(editorState, newContentState, 'remove-range');
+        return removeSelectedBlocksStyle(newState);
+    };
+
+    function getCurrentBlock(editorState: EditorState) {
+        const currentSelection = editorState.getSelection();
+        const blockKey = currentSelection.getStartKey();
+        return editorState.getCurrentContent().getBlockForKey(blockKey);
+    }
+
+    function getCurrentLetter(editorState: EditorState) {
+        const currentBlock = getCurrentBlock(editorState);
+        const blockText = currentBlock.getText();
+        return blockText[editorState.getSelection().getStartOffset() - 1];
+    }
+
+    const handleEditorStateChange = (newEditorState: EditorState) => {
+        const letter = getCurrentLetter(newEditorState);
+        const lastChangedType = newEditorState.getLastChangeType();
+
+        if (!letter && lastChangedType === 'backspace-character') {
+            const reset = getResetEditorState(editorState);
+            setEditorState(reset);
+            return;
+        }
+
+        setEditorState(newEditorState);
     };
 
     return (
-        <CKEditor
-            data={value}
-            onChange={handleChange}
+        <Editor
+            editorState={editorState}
+            toolbarClassName="draft-js-editor-toolbar"
+            editorClassName="draft-js-editor-container"
+            wrapperClassName="draft-js-container"
+            onEditorStateChange={handleEditorStateChange}
+            toolbar={{
+                options: ['blockType', 'inline', 'link', 'list', 'history'],
+                inline: {
+                    inDropdown: false,
+                    options: ['bold', 'italic'],
+                    className: 'draft-js-editor-toolbar-separator',
+                },
+                list: {
+                    inDropdown: false,
+                    options: ['unordered', 'ordered'],
+                    className: 'draft-js-editor-toolbar-separator',
+                },
+                history: {
+                    className: 'draft-js-editor-toolbar-separator',
+                },
+                link: {
+                    inDropdown: false,
+                    options: ['link'],
+                    className: 'draft-js-editor-toolbar-separator',
+                },
+                blockType: {
+                    inDropdown: true,
+                    options: ['Normal', 'H1', 'H2', 'H3', 'Blockquote'],
+                },
+            }}
+            placeholder={props.placeholder}
             onBlur={handleBlur}
-            editor={Editor}
-            config={editorConfiguration}
-            disabled={props.disabled}
+            localization={{ locale: 'ar', translations: editorLabels }}
         />
     );
 };
 
+const editorLabels = {
+    'components.controls.blocktype.h1': 'Heading 1',
+    'components.controls.blocktype.h2': 'Heading 2',
+    'components.controls.blocktype.h3': 'Heading 3',
+    'components.controls.blocktype.blockquote': 'Blockquote',
+    'components.controls.blocktype.normal': 'Paragraph',
+};
 export default MarkdownEditor;
