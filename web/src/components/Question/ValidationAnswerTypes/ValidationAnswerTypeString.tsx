725eb714376520bc2159910576a95668cb0ad9cf
Arezoo Ghalichi <arezoo.ghalichi@nhn.no>
2024-01-25 14:52:09 +0100

    #318031 ny regex regel til folkeregistering

--- web/src/components/Question/ValidationAnswerTypes/ValidationAnswerTypeString.tsx
+++ web/src/components/Question/ValidationAnswerTypes/ValidationAnswerTypeString.tsx
@@ -36,11 +36,15 @@ const ValidationAnswerTypeString = ({ item }: Props): JSX.Element => {
             code: '^((\\+|00)(\\d{1,3}))?\\d{5,12}$',
         },
         {
+            display: t('Characters approved in the national register (used for names and addresses)'),
+            code: "[\x00-\x7F\xA0-\xFF-ČĐŊŠŦŽčđŋšŧž'’]+$"
+        },
+        {
             display: t('Only norwegian characters'),
             code: '^[æøåÆØÅa-zA-Z ]*$',
         },
         {
-            display: t('Only norwegian characters + hyphen and space (used for names)'),
+            display: t('Only norwegian characters + hyphen and space'),
             code: '^[æøåÆØÅa-zA-Z\\- ]*$',
         },
         {
