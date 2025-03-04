25ea40ca2cb4500f72a3e84791027fcd6fd99c40
Eirik Søreng <eirik.soreng@nhn.no>
2025-03-04 11:03:07 +0100

    Fjerner unødvendig gml. global miljø setup for test #346474

--- web/setupTests.ts
+++ web/setupTests.ts
@@ -3,14 +3,3 @@ import "@testing-library/jest-dom/vitest";
 import "./__mocks__/matchMedia.ts";
 import "./__mocks__/IntersectionObserver.ts";
 import "./__mocks__/scrollTo.ts";
-
-// @ts-expect-error global is not defined
-global.IS_REACT_ACT_ENVIRONMENT = true;
-// @ts-expect-error global is not defined
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-// @ts-expect-error global is not defined
-self.IS_REACT_ACT_ENVIRONMENT = true;
-// @ts-expect-error global is not defined
-window.IS_REACT_ACT_ENVIRONMENT = true;
-// @ts-expect-error global is not defined
-this.IS_REACT_ACT_ENVIRONMENT = true;
