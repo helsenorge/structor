643c924fdf99666216dde8b0d57fc3b73ae178b7
ruben <rubenr@aboveit.no>
2025-05-09 14:12:55 +0200

    rollback

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import "./components/Refero/styles/refero.scss";
 
 import "./App.css";
 
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import GetRoutes from "./router/index";
 import { TreeContextProvider } from "./store/treeStore/treeStore";
@@ -10,7 +10,7 @@ import { TreeContextProvider } from "./store/treeStore/treeStore";
 function App(): JSX.Element {
   return (
     <TreeContextProvider>
-      <Router>
+      <Router basename="/static_skjemabygger">
         <GetRoutes />
       </Router>
     </TreeContextProvider>
