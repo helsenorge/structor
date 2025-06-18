81d5a020852ac5e1e982556b7518f0c0c00ccf1c
ruben <rubenr@aboveit.no>
2025-06-18 13:21:09 +0200

    https->https-system

--- web/src/types/IQuestionnareItemType.ts
+++ web/src/types/IQuestionnareItemType.ts
@@ -97,7 +97,7 @@ export enum ICodeSystem {
   sliderDisplayType = "http://helsenorge.no/fhir/CodeSystem/SliderDisplayType",
   validationOptions = "http://helsenorge.no/fhir/CodeSystem/ValidationOptions",
   workflow = "http://helsenorge.no/fhir/CodeSystem/workflow",
-  contextParameter = "https://helsenorge.no/fhir/ContextParameter",
+  contextParameter = "http://helsenorge.no/fhir/ContextParameter",
 }
 
 export enum IExtensionType {
