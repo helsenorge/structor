c8979839329d790cd0753d67bbcddaf962369f0a
Reuben Ringdal <87015861+ruberino@users.noreply.github.com>
2025-12-05 13:07:11 +0100

    chore(eslint): disable react-refresh/only-export-components and react-hooks/exhaustive-deps

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,16 +1,35 @@
-import config from '@helsenorge/eslint-config';
+import config from "@helsenorge/eslint-config";
 
 export default [
-    ...config,    
-    {
-        rules: {
-          '@typescript-eslint/no-unused-expressions': 0,
-          '@typescript-eslint/no-unused-vars': 0,
+  ...config,
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      // Relax rules that cause too many false positives in this project
+      "react-refresh/only-export-components": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          disallowTypeAnnotations: false,
+          fixStyle: "separate-type-imports",
         },
-      },
-      {
-        ignores: [
-          '**/__tests__/*',
-        ],
-      },
-  ];
+      ],
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+  {
+    files: ["**/*.tsx"],
+    rules: {
+      "react-refresh/only-export-components": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/consistent-type-imports": "off",
+    },
+  },
+
+  {
+    ignores: ["**/__tests__/*", "**/tests/**"],
+  },
+];
